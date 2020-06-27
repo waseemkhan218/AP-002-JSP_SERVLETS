dd5ae2ffa908d18448cf225a98b5b1e3a7e2069b
Waseem Khan Mohammad <mwkhan094@gmail.com>
2020-06-27 15:39:06 -0400

    Different methods for httpservlets

--- src/com/learning/AddServlet.java
+++ src/com/learning/AddServlet.java
@@ -10,14 +10,18 @@ import javax.servlet.http.HttpServletResponse;
 public class AddServlet extends HttpServlet {
 	
 	// We need to use service method only as it's a servlet life cycle.
+	// HttpServletRequest and HttpServletResponse is a request and response 
 
-	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		int i = Integer.parseInt(req.getParameter("num1"));
 		int j = Integer.parseInt(req.getParameter("num2"));
 		
 		int k = i + j;
 		
-//		System.out.println("result of k: " + k);
+		// System.out.println("result of k: " + k);
+		
+		// PrintWriter object helps to print on the response object
+		// It throws error which needs to be handled.
 		
 		PrintWriter out = res.getWriter();
 		
