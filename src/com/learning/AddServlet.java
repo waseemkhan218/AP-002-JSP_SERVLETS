0512d0bcdf3f797e5b1c41ba775c41a631d2f8aa
Waseem Khan Mohammad <mwkhan094@gmail.com>
2020-06-27 16:55:40 -0400

    Calling servlet from servlet

--- src/com/learning/AddServlet.java
+++ src/com/learning/AddServlet.java
@@ -3,6 +3,8 @@ package com.learning;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,6 +13,8 @@ public class AddServlet extends HttpServlet {
 	
 	// We need to use service method only as it's a servlet life cycle.
 	// HttpServletRequest and HttpServletResponse is a request and response 
+	
+	// We can use doGet and doPost. Those are methods provides from httpServlets.
 
 	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		int i = Integer.parseInt(req.getParameter("num1"));
@@ -27,5 +31,20 @@ public class AddServlet extends HttpServlet {
 		
 		out.println("result of k:" + k);
 	}
-
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num2"));
+		
+		int k = i + j;
+		
+		// we need to send data to other servlet. We have few ways of doing it. 
+		// Here we are passing the req from addServlet directly to SquareServlet 
+		
+		req.setAttribute("k", k);
+		
+		RequestDispatcher rd = req.getRequestDispatcher("sq");
+		rd.forward(req, res);
+	}
+	
 }
---
+++ src/com/learning/SquareServlet.java
@@ -0,0 +1,22 @@
+package com.learning;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SquareServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		
+		int k = (int) req.getAttribute("k"); 
+		
+		k = k * k;
+		
+		PrintWriter out = res.getWriter();
+		
+		out.println("result is K: " + k);
+	}
+}
