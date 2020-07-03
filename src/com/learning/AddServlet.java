0f723bde1536b35dd5f7acecb15c62a2dec46504
Waseem Khan Mohammad <mwkhan094@gmail.com>
2020-07-03 12:04:33 -0400

    Adding annotation based configuration

--- src/com/learning/AddServlet.java
+++ src/com/learning/AddServlet.java
@@ -5,12 +5,14 @@ import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+@WebServlet("/add")
 public class AddServlet extends HttpServlet {
 	
 	// We need to use service method only as it's a servlet life cycle.
--- src/com/learning/MyServlet.java
+++ src/com/learning/MyServlet.java
@@ -17,6 +17,15 @@ public class MyServlet extends HttpServlet {
 		PrintWriter out = response.getWriter();
 		out.print("Hi ");
 		
+		// Servlet Context is used if you have some config like environment key and value which are common for all
+		// the servlet in the app.
+		
+//		ServletContext ctx = getServletContext();
+//		String str = ctx.getInitParameter("phone");
+//		out.println(str);
+		
+		// Servlet Config is used if you want to have some config for specific servlet.
+		
 		ServletConfig conf = getServletConfig();
 		String str = conf.getInitParameter("name");
 		out.println(str);
