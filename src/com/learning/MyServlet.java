803e102af890d82ac2f939532ea9641a493d0cba
Waseem Khan Mohammad <mwkhan094@gmail.com>
2020-07-03 11:58:06 -0400

    Adding servletContext and servletConfig

---
+++ src/com/learning/MyServlet.java
@@ -0,0 +1,25 @@
+package com.learning;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			
+		PrintWriter out = response.getWriter();
+		out.print("Hi ");
+		
+		ServletConfig conf = getServletConfig();
+		String str = conf.getInitParameter("name");
+		out.println(str);
+	}
+
+}
